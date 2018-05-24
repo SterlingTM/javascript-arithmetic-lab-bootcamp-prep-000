fd29fa2c74824bebd3d5b85fc6463246bb2facfa
Sterling Moss <mtsilvertm@gmail.com>
2018-05-24 17:22:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,15 @@ function inc(n) {
 function dec(n) {
   return n-= 1;
 }
+<<<<<<< HEAD
 function makeInt(string) {
  return parseInt(string, 10);
 }
 function preserveDecimal(string) {
  return parseFloat(string);
 }
+=======
+function parseInt(string, int) {
+return;
+}
+>>>>>>> a9ab4eb0852f076eb665273f0d212f607dfc5a74
